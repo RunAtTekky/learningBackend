4111d5cf8dc73017b6b483534d3c1b5d2c46e23d
Varun <varunrawat343@gmail.com>
2024-06-05 16:46:11 +0530

    app dot get

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ import express from "express";
 const app = express();
 const port = 5000;
 
+app.get("/getProducts", (req, res) => {
+  res.json({
+    success: true,
+    products: ["football", "net", "goalpost"],
+  });
+});
+
 app.listen(port, () => {
   console.log(`Server is running on ${port}`);
 });
