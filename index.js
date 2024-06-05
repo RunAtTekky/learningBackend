9e2bb57c10eef215f6af6b10c49caad030621d37
Varun <varunrawat343@gmail.com>
2024-06-05 15:05:21 +0530

    Async file reading

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 import http from "http";
 import { generateLovePercent } from "./features.js";
+import fs from "fs";
 
 console.log(generateLovePercent());
 
 const server = http.createServer((req, res) => {
   console.log(req.url);
-  if (req.url === "/about") {
+  if (req.url === "/home") {
+    fs.readFile("./index.html", (err, home) => {
+      console.log("File Read");
+      res.end(home);
+    });
+  } else if (req.url === "/about") {
     res.end("<h1>About</h1>");
   } else if (req.url === "/love") {
     res.end(`<h1>Love is ${generateLovePercent()}</h1>`);
