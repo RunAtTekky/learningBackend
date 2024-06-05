5fcadd097fdc546f084b0070dcfd457e5a0225f0
Varun <varunrawat343@gmail.com>
2024-06-05 14:34:13 +0530

    Basic server working

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hello World")
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  console.log(req.url);
+  res.end("<h1>Noice</h1>");
+});
+
+server.listen(5000, () => {
+  console.log("Server is working");
+});
