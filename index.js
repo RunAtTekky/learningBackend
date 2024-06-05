ae4d89ca791eb8e597c9ecc65d532d35a9c04026
Varun <varunrawat343@gmail.com>
2024-06-05 17:34:11 +0530

    Add form and routing

--- index.js
+++ index.js
@@ -2,13 +2,34 @@ import express from "express";
 import path from "path";
 
 const app = express();
+const users = [];
+
+app.use(express.static(path.join(path.resolve(), "public")));
+app.use(express.urlencoded({ extended: true }));
 
 // Setting up view engine
 app.set("view engine", "ejs");
 const port = 5000;
 
 app.get("/", (req, res) => {
-  res.render("index", { name: "Varun" });
+  res.render("index");
+});
+
+app.get("/success", (req, res) => {
+  res.render("success");
+});
+
+app.post("/contact", (req, res) => {
+  console.log(req.body);
+  users.push({ userName: req.body.name, email: req.body.email });
+
+  res.redirect("/success");
+});
+
+app.get("/users", (req, res) => {
+  res.json({
+    users,
+  });
 });
 
 app.listen(port, () => {
