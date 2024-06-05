dbaa1ee6dada2834ad38c99255dab2e4a927af49
Varun <varunrawat343@gmail.com>
2024-06-05 16:36:49 +0530

    Change to express

--- index.js
+++ index.js
@@ -1,23 +1,8 @@
-import http from "http";
-import { generateLovePercent } from "./features.js";
-import fs from "fs";
+import express from "express";
 
-console.log(generateLovePercent());
+const app = express();
+const port = 5000;
 
-const home = fs.readFileSync("./index.html");
-
-const server = http.createServer((req, res) => {
-  console.log(req.url);
-  console.log(req.method);
-  if (req.url === "/home") {
-    res.end(home);
-  } else if (req.url === "/about") {
-    res.end("<h1>About</h1>");
-  } else if (req.url === "/love") {
-    res.end(`<h1>Love is ${generateLovePercent()}</h1>`);
-  }
-});
-
-server.listen(5000, () => {
-  console.log("Server is working");
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`);
 });
